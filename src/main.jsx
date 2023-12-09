7ddd3063ca0a40d217f9268acd8412deec03829e
Lauren Thorud <lthorud12@gmail.com>
2023-12-09 03:29:27 -0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,35 @@ import { BrowserRouter } from 'react-router-dom'
 import './assets/scss/styles.scss';
 import 'remixicon/fonts/remixicon.css';
 import { AuthProvider } from './context/useAuth.js';
+import { tryLoadAndStartRecorder } from '@alwaysmeticulous/recorder-loader'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+const METICULOUS_SAMPLING_RATE = 0.5;
+
+async function startApp() {
+  // Record all sessions on localhost, staging stacks and preview URLs
+  if (!isProduction()) {
+    // Start the Meticulous recorder before you initialise your app.
+    // Note: all errors are caught and logged, so no need to surround with try/catch
+    await tryLoadAndStartRecorder({
+      projectId: '3yTGMkw7NSboq11GzixoYCFTNvws3iBQEULp3Se5',
+      isProduction: false,
+    });
+  }
+  
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </React.StrictMode>,
+  )
+}
+
+function isProduction() {
+  return window.location.hostname.indexOf('tarotlyfe.com') > -1
+}
+
+startApp()
